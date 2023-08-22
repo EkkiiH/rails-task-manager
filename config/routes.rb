e0e2d701d45aaeb7c39cd24d8f54e85a6532589f
Zyania Alvarez <zyania.alvarez@gmail.com>
2023-08-22 16:03:51 +0200

    funcitional task manager, needs to DRY and front-end

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get "tasks", to: "tasks#index"
+  # # create task
+  # get "tasks/new", to: "tasks#new"
+  # # show a task
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # post "tasks", to: "tasks#create"
+  # # edit a task
+  # get "tasks/:id/edit", to: "tasks#edit"
+  # patch "tasks/:id", to: "tasks#update"
+  # # delete a task
+  # delete "tasks/:id", to: "tasks#destroy"
+
+  resources :tasks
 end
